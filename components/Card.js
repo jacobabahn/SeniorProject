b55fb30ae20b7e3c9dd1841b4a14ad8500a3d555
Jacob Bahn <jacobabahn@gmail.com>
2022-02-22 10:46:33 -0500

    Added ellipsis to title and description of cards

--- components/Card.js
+++ components/Card.js
@@ -6,17 +6,17 @@ const Card = (props) => {
         <Center>
             <Box style={styles.body} p="5" h="auto" w ="90%" bg="dark.100" m="4%" rounded="2xl" shadow="3">
                 <Box style={styles.container}>
-                    <Text py="2" px="3" color="white" fontSize="2xl" h="55px">{props.title}</Text>
+                    <Text style={styles.title} numberOfLines={1} py="2" px="3" color="white" fontSize="2xl" >{props.title}</Text>
                     <Text w="95%" pl="2" color="lightgray" fontSize="md">{props.description}</Text>
                 </Box>
                     <Box style={styles.rounded} bg="info.700" rounded="xl" mb="5" mt="5" mx="3" shadow="3" p="3">
-                        <Text h="30px" maxh="30px" overflow="scroll" color="white" fontSize="2xl">3:56</Text>
+                        <Text numberOfLines={2} h="30px" overflow="scroll" color="white" fontSize="2xl">3:56</Text>
                     </Box>
             </Box>
         </Center>
     )
 }
-
+7
 const styles = StyleSheet.create({
     body: {
         flexDirection: "row",
@@ -28,7 +28,7 @@ const styles = StyleSheet.create({
     rounded: {
         // display: "flex",
         alignItems: "center",
-    }
+    },
 })
 
 export default Card
